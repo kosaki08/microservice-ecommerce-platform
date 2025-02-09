3895e16a017fbf8b1347853c7625a7909d835b03
Kazunori Osaki <claudioropez.k@gmail.com>
2025-02-09 17:29:17 +0900

    type-checkコマンドを追加

---
+++ packages/eslint-config/index.d.ts
@@ -0,0 +1,20 @@
+declare module "@portfolio-2025/eslint-config/next-js" {
+  namespace NextJsESLintConfig {
+    const config: any;
+  }
+  export default NextJsESLintConfig.config;
+}
+
+declare module "@portfolio-2025/eslint-config/react-internal" {
+  namespace ReactInternalESLintConfig {
+    const config: any;
+  }
+  export default ReactInternalESLintConfig.config;
+}
+
+declare module "@portfolio-2025/eslint-config/nest" {
+  namespace NestESLintConfig {
+    const config: any;
+  }
+  export default NestESLintConfig.config;
+}
---
+++ packages/eslint-config/nest.js
@@ -0,0 +1,51 @@
+import js from "@eslint/js";
+import eslintConfigPrettier from "eslint-config-prettier";
+import pluginNest from "eslint-plugin-nestjs";
+import tseslint from "typescript-eslint";
+import { config as baseConfig } from "./base.js";
+
+/**
+ * A custom ESLint configuration for NestJS applications.
+ *
+ * @type {import("eslint").Linter.Config}
+ * */
+export const nestConfig = [
+  ...baseConfig,
+  js.configs.recommended,
+  eslintConfigPrettier,
+  ...tseslint.configs.recommended,
+  {
+    plugins: {
+      "@nestjs/eslint-plugin": pluginNest,
+    },
+    rules: {
+      ...pluginNest.configs.recommended.rules,
+      // NestJS特有のルール
+      "@nestjs/use-dependency-injection": "error",
+      "@nestjs/use-validation-pipe": "error",
+    },
+  },
+  {
+    extends: ["plugin:@typescript-eslint/recommended", "plugin:@typescript-eslint/recommended-requiring-type-checking"],
+    rules: {
+      // クラスメソッドやプロパティのアクセス修飾子を必須に
+      "@typescript-eslint/explicit-member-accessibility": ["error"],
+      // 戻り値の型を明示的に指定することを推奨
+      "@typescript-eslint/explicit-function-return-type": ["error"],
+      // 未使用の変数をエラーに
+      "@typescript-eslint/no-unused-vars": ["error"],
+      // any型の使用を制限
+      "@typescript-eslint/no-explicit-any": "warn",
+    },
+  },
+  {
+    env: {
+      node: true,
+      jest: true, // テスト用
+    },
+    parserOptions: {
+      project: "tsconfig.json",
+      sourceType: "module",
+    },
+  },
+];
