cfcc907aae039fcd1d0489c0871be280774480e3
Kazunori Osaki <claudioropez.k@gmail.com>
2025-02-09 16:56:25 +0900

    prettier-configパッケージを追加

--- apps/docs/app/page.tsx
+++ apps/docs/app/page.tsx
@@ -1,5 +1,5 @@
 import Image, { type ImageProps } from "next/image";
-import { Button } from "@repo/ui/button";
+import { Button } from "@portfolio-2025/ui/button";
 import styles from "./page.module.css";
 
 type Props = Omit<ImageProps, "src"> & {
