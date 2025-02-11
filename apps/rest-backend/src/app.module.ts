26d1b243229a4519a845b5653194760f129a7111
Kazunori Osaki <claudioropez.k@gmail.com>
2025-02-12 01:04:36 +0900

    ヘルスチェック用のエンドポイントを追加

--- apps/rest-backend/src/app.module.ts
+++ apps/rest-backend/src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from "@nestjs/common";
+import { HealthModule } from ".//health/health.module";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { MonitoringModule } from "./monitoring/monitoring.module";
 
 @Module({
-  imports: [MonitoringModule],
+  imports: [MonitoringModule, HealthModule],
   controllers: [AppController],
   providers: [AppService],
 })
