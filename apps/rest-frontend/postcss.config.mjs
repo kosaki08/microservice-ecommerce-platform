233188ff037fe3681bc1edac618e1d34b0d36d5b
Kazunori Osaki <claudioropez.k@gmail.com>
2025-02-17 01:01:26 +0900

    shadcn/uiの設定を追加

--- apps/rest-frontend/postcss.config.mjs
+++ apps/rest-frontend/postcss.config.mjs
@@ -1,8 +1,1 @@
-/** @type {import('postcss-load-config').Config} */
-const config = {
-  plugins: {
-    tailwindcss: {},
-  },
-};
-
-export default config;
+export { default } from "@portfolio-2025/ui/postcss.config";
